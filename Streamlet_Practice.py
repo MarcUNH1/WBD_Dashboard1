5f694387d2dedceb4a8d667e8c8dafc69b5a38e5
MarcUNH1 <marc.emond@unh.edu>
2026-05-03 17:41:43 -0400

    Add files via upload

--- Streamlet_Practice.py
+++ Streamlet_Practice.py
@@ -60,7 +60,7 @@ with col[2]:
 
     t1.subheader("Wind Speed")
     fig_wspd = px.line(data, x="Time", y="wSPD")
-    t1.plotly_chart(fig_wspd, use_container_width=True, height=500, dtick="H1")
+    t1.plotly_chart(fig_wspd, use_container_width=True, height=500)
 
     t2.subheader("Wind Direction")
     fig_wdir = px.line(data, x="Time", y="wDIR")
