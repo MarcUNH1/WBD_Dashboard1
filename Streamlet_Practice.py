d63a57c4f15fbc2ad6e25648cd947e3f14d5ab03
MarcUNH1 <marc.emond@unh.edu>
2026-05-03 20:37:21 -0400

    Add files via upload

--- Streamlet_Practice.py
+++ Streamlet_Practice.py
@@ -55,11 +55,11 @@ with col[2]:
     t1, t2, t3, t4, t5, t6, t7, t8 = st.tabs(["WSPD", "WDIR", "AirT", "AirP", "rH", "WaterT", "SigH", "Tp1"])
     all_timestamps = []
     all_timestamps.append(data['Time'])
-
+    print(data['Time'].max())
     t1.subheader("Wind Speed")
     fig_wspd = px.line(data, x="Time", y="wSPD")
-    fig_wspd.update_xaxes(autorangeoptions_maxallowed=data['Time'].max())
-    fig_wspd.update_xaxes(autorangeoptions_minallowed=data['Time'].min())
+    fig_wspd.update_xaxes(maxallowed=data['Time'].max())
+    fig_wspd.update_xaxes(minallowed=data['Time'].min())
 
     t1.plotly_chart(fig_wspd, use_container_width=True, height=500)
 
